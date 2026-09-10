3f1cf02fa54bb1a9f15462f4a02247a793506fc6
Wildan M Zaki <wildanmzaki7@gmail.com>
2026-09-10 08:09:50 +0700

    feat: note jawaban dari soal yang disertakan sebelum nanti disertakan juga di laporan

--- Soal2_Constants2.java
+++ Soal2_Constants2.java
@@ -8,3 +8,34 @@ public class Soal2_Constants2 {
                 CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
     }
 }
+
+/**
+ * Dari 2 contoh baris program diatas, jawablah pertanyaan dibawah ini:
+ * 1. Bagaimana output dari masing masing class Constants dan Constants2?
+ * 2. Apa perbedaan penggunaan final double dengan public static final double?
+ * 
+ * Jawaban:
+ * 1. Dari hasil uji coba, tidak ada perbedaan signifikan. Keduanya sama-sama
+ * menghasilkan output: "Paper size in centimeters: 21.59 by 27.94"
+ * Bukti Terminal Runtime:
+ * java-onboarding on  master [✘?] via ☕ v21.0.5 took 15s
+ * ❯ javac Soal2_Constants.java
+ * 
+ * java-onboarding on  master [✘?] via ☕ v21.0.5
+ * ❯ java Soal2_Constants
+ * Paper size in centimeters: 21.59 by 27.94
+ * 
+ * java-onboarding on  master [✘?] via ☕ v21.0.5
+ * ❯ javac Soal2_Constants2.java
+ * 
+ * java-onboarding on  master [✘?] via ☕ v21.0.5
+ * ❯ java Soal2_Constants2
+ * Paper size in centimeters: 21.59 by 27.94
+ * 
+ * 2. Perbedaan keduanya terletak pada scope access dari constanta tersebut.
+ * Di mana final double yang di-declare di main method itu hanya accessible di dalam
+ * scope method main itu saja
+ * Sementara untuk public static final double itu scope access-nya adalah sebagai
+ * class attribute di mana dia juga accessible di methods lain dalam satu class yang
+ * sama
+ */

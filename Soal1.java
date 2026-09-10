a995716817d62ee378866085fd53ea247ef7eb61
Wildan M Zaki <wildanmzaki7@gmail.com>
2026-09-10 08:04:46 +0700

    feat: rename to better redaction

--- Soal1.java
+++
@@ -1,9 +0,0 @@
-public class Soal1 {
-    public static void main(String[] args) {
-        int angka1 = 125;
-        int angka2 = 6;
-        int hasil = (int) (angka1+angka2);
-
-        System.out.println("Hasil: " + hasil);
-    }
-}
